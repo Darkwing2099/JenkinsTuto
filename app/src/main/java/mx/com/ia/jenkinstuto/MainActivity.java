354be2c79ad4dbb5b81844eaf4df2dd33d64871b
Armin Rodriguez <arodriguez@ia.com.mx>
2018-11-08 16:00:09 -0600

    inicio de proyecto

---
+++ app/src/main/java/mx/com/ia/jenkinstuto/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.com.ia.jenkinstuto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
